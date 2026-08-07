e3e81c5f8aa0863a72c790ba8a93952d2f46058e
SOKLEAP <phornzokl3ap@gmail.com>
2026-08-08 00:16:57 +0700

    08/08/2026

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,80 @@ const router = express.Router();
 router.get('/user', async (req, res) => {
     try {
         const users = await user_model.find();
-        res.json(users);
+        res.status(200).json(users);
     } catch (error) {
         res.status(500).json({
+            message: error.message
+        })
+    }
+});
+
+router.post('/user', async (req, res) => {
+    try {
+        // create users object
+        const users = new user_model(req.body);
+        await users.save();
+        res.status(201).json(users);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+});
+
+router.get('/user/:id', async (req, res) => {
+    try {
+        const users = await user_model.findById(req.params.id);
+
+        if(!users){
+            return res.status(404).json({
+                message: "User not found!"
+            })
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+});
+
+router.put('/user/:id', async (req, res) => {
+    try {
+        const users = await user_model.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+
+        if(!users){
+            return res.status(404).json({
+                message: "User not found!"
+            });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+});
+
+router.delete('/user/:id', async (req, res) => {
+    try {
+        const users = await user_model.findByIdAndDelete(req.params.id);
+
+        if(!users){
+            return res.status(404).json({
+                message: "User not found!"
+            });
+        }
+
+        res.status(200).json({
+            message: "User deleted successful."
+        });
+    } catch (error) {
+        res.status(404).json({
             message: error.message
         })
     }
